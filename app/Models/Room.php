145b8c7c7e6ac8dc1a73734001b767aa90243424
sahid123502 <sahidhdyt123@gmail.com>
2026-07-30 21:31:31 +0700

    Delete app directory

--- app/Models/Room.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-
-class Room extends Model
-{
-    protected $fillable = ['room_number', 'type', 'price', 'status', 'facilities'];
-    public function reservations() { return $this->hasMany(Reservation::class); }
-}
